cb08bbf4858f757d42be59c2ad3480ef384c774f
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-08-01 05:15:27 +0900

    add Route For Minuman and fix route name

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\MenuMakananController;
+use App\Http\Controllers\API\MenuMinumanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
@@ -32,8 +33,11 @@ Route::apiResource('proyek', ProyekController::class) -> middleware('auth:api');
  * -> middleware('auth:api')
  * */
 //Route::apiResource('makananlist' , MenuMakananController::class);
-Route::post('makananlist' , [MenuMakananController::class, 'store']) -> middleware('auth:api');
-Route::get('makananlist' , [MenuMakananController::class, 'index']);
-Route::get('makananlist/{id}' , [MenuMakananController::class, 'show']);
-Route::patch('makananlist/{id}' , [MenuMakananController::class, 'update']) -> middleware('auth:api');
-Route::delete('makananlist/{id}' , [MenuMakananController::class, 'destroy']) -> middleware('auth:api');
+Route::post('makanan' , [MenuMakananController::class, 'store']) -> middleware('auth:api');
+Route::get('makanan' , [MenuMakananController::class, 'index']);
+Route::get('makanan/{id}' , [MenuMakananController::class, 'show']);
+Route::patch('makanan/{id}' , [MenuMakananController::class, 'update']) -> middleware('auth:api');
+Route::delete('makanan/{id}' , [MenuMakananController::class, 'destroy']);
+
+
+Route::apiResource('minuman', MenuMinumanController::class);
