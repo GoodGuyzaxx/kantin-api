76fa6343afd2a4cbe7920663152678c967da8815
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-08-04 04:05:32 +0900

    add: Handler Payment Notif Midtrans

---
+++ app/Http/Controllers/HandlerPaymentNotifController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\PaymentHistory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Testing\Fluent\Concerns\Has;
+use phpseclib3\Crypt\Hash;
+
+class HandlerPaymentNotifController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $payload = $request->all();
+
+        Log::info('incoming-midtrans', [
+            'payload' => $payload
+        ]);
+
+        $orderId = $payload['order_id'];
+        $statusCode = $payload['status_code'];
+        $grossAmount = $payload['gross_amount'];
+
+        $reqSignatureKey = $payload['signature_key'];
+
+        $signature = hash('sha512', $orderId.$statusCode.$grossAmount.config('midtrans.key'));
+
+        if($signature != $reqSignatureKey) {
+            return response()->json([
+                'message' => 'invalid signature'
+            ],401);
+        }
+
+        $transactionStatus = $payload['transaction_status'];
+
+        PaymentHistory::create([
+            'order_id' => $orderId,
+            'status' => $transactionStatus,
+            'payload' => json_encode($payload)
+        ]);
+
+        $order = Order::find($orderId);
+        if (!$order) {
+            return response()->json([
+                'message' => 'invalid Oreder'
+            ],400);
+        }
+
+        if($transactionStatus == 'settlement') {
+           $order->status = 'PAID';
+           $order->save();
+        }else if($transactionStatus == 'expire') {
+            $order->status = 'EXPIRED';
+            $order->save();
+        }
+
+        return response()->json([
+            'message' => 'Success'
+        ]);
+    }
+}
