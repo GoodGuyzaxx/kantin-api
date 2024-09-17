0d95f8a7c05d9976ce08edf585258a3896d6774e
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-09-18 04:42:27 +0900

    add : Route for Rating, transaksi, and fix kategori route

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\API\RatingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ProyekController;
+use App\Http\Controllers\API\OrderPaymentController;
+use App\Http\Controllers\HandlerPaymentNotifController;
 use App\Http\Controllers\API\KonsumenController;
 use App\Http\Controllers\API\AdminController;
+use App\Http\Controllers\API\TransaksiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,19 +35,24 @@ Route::get('register', [AuthController::class, 'index']);
 
 Route::post('konsumen/register', [KonsumenController::class, 'register']);
 Route::post('konsumen/login', [KonsumenController::class, 'login']) -> name('login');
+route::patch('konsumen/{id}', [KonsumenController::class,'update']);
 
 Route::post('admin/login', [AdminController::class, 'login']) ->name('login');
 Route::post('admin/register', [AdminController::class, 'register']);
 
 Route::get('rating', [RatingController::class, 'index']);
 Route::post('rating', [RatingController::class, 'store']);
+Route::get('rating/{id_konsumen}/{id_menu}', [RatingController::class, 'indexById']);
+Route::patch('rating/{id_konsumen}/{id_menu}', [RatingController::class, 'updateRating']);
+
+Route::post('transaksi', [TransaksiController::class, 'store']);
 
 Route::get('kantin', [KantinController::class, 'index']);
 
-Route::get('orders/{email}', [\App\Http\Controllers\API\OrderPaymentController::class, 'getByEmail']);
-Route::get('order/id/{id}', [\App\Http\Controllers\API\OrderPaymentController::class, 'showOrderId']);
-Route::post('order/buy', [\App\Http\Controllers\API\OrderPaymentController::class, 'buy']);
-Route::post('midtrans/notif-hook',[\App\Http\Controllers\HandlerPaymentNotifController::class,'__invoke']);
+Route::get('orders/{email}', [OrderPaymentController::class, 'getByEmail']);
+Route::get('order/id/{id}', [OrderPaymentController::class, 'showOrderId']);
+Route::post('order/buy', [OrderPaymentController::class, 'buy']);
+Route::post('midtrans/notif-hook',[HandlerPaymentNotifController::class,'__invoke']);
 
 //Menu Route
 /* add this code for req token
@@ -60,6 +67,6 @@ Route::patch('menu/{id}', [MenuController::class, 'update']);
 Route::get('menu', [MenuController::class, 'index']);
 Route::get('menu/{id}', [MenuController::class, 'show']);
 Route::get('menu/kantin/{id}', [MenuController::class, 'indexByIdKantin']);
-Route::get('{kategori}',[MenuController::class, 'indexByKategori']);
-Route::get('{ketegori}/kantin/{id}', [MenuController::class, 'indexMenuWithId']);
-Route::get('{ketegori}/kantin/{id}', [MenuController::class, 'indexMenuWithId']);
+Route::get('kategori/{kategori}',[MenuController::class, 'indexByKategori']);
+Route::get('kategori/{ketegori}/kantin/{id}', [MenuController::class, 'indexMenuWithId']);
+Route::get('kategori/{ketegori}/kantin/{id}', [MenuController::class, 'indexMenuWithId']);
