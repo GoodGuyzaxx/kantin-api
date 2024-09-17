8a28574b0e4e613e3816ab4877f1fb10a18f09fc
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-09-18 04:40:51 +0900

    add : update function and status message

--- app/Http/Controllers/API/KonsumenController.php
+++ app/Http/Controllers/API/KonsumenController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Konsumen;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -49,6 +50,7 @@ class KonsumenController extends Controller
         return response([
             'success' => true,
             'message' => 'berhasil login',
+            'status' => 'konsumen',
             'data' => [
                 'id_konsumen' => $user->id_konsumen,
                 'nama_konsumen' => $user->nama_konsumen,
@@ -58,5 +60,16 @@ class KonsumenController extends Controller
         ]);
 
 
+    }
+
+    public function update(Request $request, $id ){
+        $dataReq = $request->all();
+        $post = Konsumen::where('id_konsumen',$id);
+
+        $post->update($dataReq);
+        return response()->json([
+            'message' => 'Berhasil Mengubah Data',
+            'data' => $dataReq
+        ],201);
     }
 }
