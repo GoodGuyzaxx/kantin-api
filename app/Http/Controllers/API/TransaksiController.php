a7b23b06b0f3d56ead4385e79b34afd326c4bcfc
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-09-18 04:41:49 +0900

    add : implement Transaksi

---
+++ app/Http/Controllers/API/TransaksiController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaksi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+
+class TransaksiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'id_kantin' => 'required',
+            'total_harga' => 'required',
+            'menu' => 'required',
+            'tipe_pembayaran' => 'required',
+            'status_pembayaran' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ]);
+        }
+
+        $dataID = Str::uuid()->toString();
+        DB::table('transaksis')->insert([
+            'id_order' => $dataID ,
+            'id_kantin' => $request->id_kantin,
+            'total_harga' => $request->total_harga,
+            'menu' => $request->menu,
+            'tipe_pembayaran' => $request->tipe_pembayaran,
+            'status_pembayaran' => $request->status_pembayaran,
+            'created_at' => now(),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Berhasil Menambah Transaksi',
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transaksi $transaksi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Transaksi $transaksi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Transaksi $transaksi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transaksi $transaksi)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_29_012038_create_transaksis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('id_order');
+            $table->foreignId('id_kantin')->constrained('kantins', 'id_kantin')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->unsignedBigInteger('total_harga');
+            $table->string('menu');
+            $table->enum('tipe_pembayaran',['tunai','non-tunai']);
+            $table->string('status_pembayaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
