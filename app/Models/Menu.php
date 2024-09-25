d094e319aaf9a635dc4497d0ab1fc669520af7bc
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-09-26 04:16:01 +0900

    Update Menu.php
    ADD : relations

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -26,4 +26,15 @@ class Menu extends Model
         'stock' => 'int'
     ];
 
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class, 'id_menu'); // 'menu_id' should match your actual foreign key
+    }
+
+    public function kantin()
+    {
+        return $this->belongsTo(Kantin::class, 'id_kantin', 'id_kantin');
+    }
+
+
 }
