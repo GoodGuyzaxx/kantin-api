f0d8b38bc4924347ec1bf5aa3a7d987c9094860b
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-09-27 15:26:36 +0900

    update Dashboar menu
    add: Count Item

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
+use App\Models\Kantin;
+use App\Models\Konsumen;
+use App\Models\Menu;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.dashboard');
+        return view('pages.admin.dashboard',[
+            'titel' => 'Dashboard',
+            'Admin' => Admin::count(),
+            'Konsumen' => Konsumen::count(),
+            'Kantin' => Kantin::count(),
+            'Menu' =>   Menu::count()
+        ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -10,69 +10,88 @@
             <!-- Content Row -->
             <div class="row">
                 <!-- Siswa Card Example -->
-{{--                <div class="col-xl-3 col-md-6 mb-4">--}}
-{{--                    <div class="card border-left-primary shadow h-100 py-2">--}}
-{{--                        <a style="text-decoration:none; color: #212529;" href="{{ route('admin.user.index') }}">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div class="row no-gutters align-items-center">--}}
-{{--                                    <div class="col mr-2">--}}
-{{--                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">--}}
-{{--                                            Siswa</div>--}}
-{{--                                        <div class="h5 mb-0 font-weight-bold text-gray-800">route</div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-auto">--}}
-{{--                                        <i class="fas fa-users fa-2x text-gray-300"></i>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <a style="text-decoration:none; color: #212529;" href="{{ route('admin.user.index') }}">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            Admin Kantin</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$Admin}}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-user-gear fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-{{--                <!-- Criteria Card Example -->--}}
-{{--                <div class="col-xl-3 col-md-6 mb-4">--}}
-{{--                    <div class="card border-left-success shadow h-100 py-2">--}}
-{{--                        <a href="{{ route('kriteria.index') }}" style="text-decoration:none; color: #212529;">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div class="row no-gutters align-items-center">--}}
-{{--                                    <div class="col mr-2">--}}
-{{--                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">--}}
-{{--                                            Kriteria</div>--}}
-{{--                                        <div class="h5 mb-0 font-weight-bold text-gray-800">route</div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-auto">--}}
-{{--                                        <i class="fas fa-table-columns fa-2x text-gray-300"></i>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <a style="text-decoration:none; color: #212529;" href="{{ route('admin.konsumen.index') }}">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            Konsumen</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$Admin}}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
-{{--                <!-- Class Card Example -->--}}
-{{--                <div class="col-xl-3 col-md-6 mb-4">--}}
-{{--                    <div class="card border-left-info shadow h-100 py-2">--}}
-{{--                        <a href="{{ route('kelas.index') }}" style="text-decoration:none; color: #212529;">--}}
-{{--                            <div class="card-body">--}}
-{{--                                <div class="row no-gutters align-items-center">--}}
-{{--                                    <div class="col mr-2">--}}
-{{--                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Kelas--}}
-{{--                                        </div>--}}
-{{--                                        <div class="row no-gutters align-items-center">--}}
-{{--                                            <div class="col-auto">--}}
-{{--                                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">route--}}
-{{--                                                </div>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-auto">--}}
-{{--                                        <i class="fas fa-school fa-2x text-gray-300"></i>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <!-- Class Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-info shadow h-100 py-2">
+                        <a href="{{ route('admin.kantin.index') }}" style="text-decoration:none; color: #212529;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Kantin
+                                        </div>
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col-auto">
+                                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$Kantin}}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-house fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <!-- Criteria Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <a href="{{ route('admin.menu.index') }}" style="text-decoration:none; color: #212529;">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                            Menu</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{$Menu}}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-book fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
 {{--                <!-- Data user Card Example -->--}}
 {{--                <div class="col-xl-3 col-md-6 mb-4">--}}
@@ -91,7 +110,7 @@
 {{--                        </div>--}}
 {{--                    </div>--}}
 {{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+            </div>
+        </div>
     </main>
 @endsection
