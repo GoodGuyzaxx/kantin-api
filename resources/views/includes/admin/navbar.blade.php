a92373560438be842250ee571630ef13443fa6f6
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2024-09-27 15:28:19 +0900

    Update navbar.blade.php
    fix : String

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #2C2A4A;">
     <!-- Navbar Brand-->
     <a class="navbar-brand ps-3" href="{{ route('admin.dashboard') }}">
-        ADMIN
+        Super Admin
     </a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
